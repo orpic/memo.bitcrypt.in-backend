2f064dc38a3d251d11faa0002e13851656c33f98
Shobhit Umrao <shobhitumrao5@gmail.com>
2025-02-05 03:04:23 +0530

    add ecocsystem config for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+const ecosystem = {
+  apps: [
+    {
+      name: 'memo.bitcrypt.in-backend',
+      script: 'dist/server.js', // Path to the entry file
+      exec_mode: 'cluster', // 'cluster' or 'fork'
+      instances: '1',
+      autorestart: true,
+      watch: false, // Set to 'true' to watch for file changes and automatically restart
+      max_memory_restart: '2G', // Restart app if it uses more than 2 GB of memory
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3020,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3020,
+      },
+    },
+  ],
+};
+
+export default ecosystem;
